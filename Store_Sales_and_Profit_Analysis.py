f5e8940c75a9aa5e6c47e7f4346d274a793644ad
tanguturiharsha-1810 <156089822+tanguturiharsha-1810@users.noreply.github.com>
2024-09-02 23:04:03 +0530

    Create Store_Sales_and_Profit_Analysis.py

---
+++ Store_Sales_and_Profit_Analysis.py
@@ -0,0 +1,97 @@
+#Let’s start this task by importing the necessary Python libraries and the dataset
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.colors as colors
+pio.templates.default = "plotly_white"
+
+data = pd.read_csv("Sample - Superstore.csv", encoding='latin-1')
+print(data.head())
+
+#Let’s start by looking at the descriptive statistics of the dataset:
+print(data.describe())
+#The dataset has an order date column. We can use this column to create new columns like order month, order year, and order day, which will be very valuable for sales and profit analysis according to time periods. So let’s add these columns:
+data['Order Date'] = pd.to_datetime(data['Order Date'])
+data['Ship Date'] = pd.to_datetime(data['Ship Date']) 
+
+data['Order Month'] = data['Order Date'].dt.month 
+data['Order Year'] = data['Order Date'].dt.year
+data['Order Day of Week'] = data['Order Date'].dt.dayofweek
+#Now let’s have a look at the monthly sales:
+sales_by_month = data.groupby('Order Month')['Sales'].sum().reset_index()
+fig = px.line(sales_by_month, 
+              x='Order Month', 
+              y='Sales', 
+              title='Monthly Sales Analysis')
+fig.show()
+#Now let’s have a look at the sales by category:
+sales_by_category = data.groupby('Category')['Sales'].sum().reset_index()
+
+
+fig = px.pie(sales_by_category, 
+             values='Sales', 
+             names='Category', 
+             hole=0.5, 
+             color_discrete_sequence=px.colors.qualitative.Pastel)
+
+fig.update_traces(textposition='inside', textinfo='percent+label')
+fig.update_layout(title_text='Sales Analysis by Category', title_font=dict(size=24))
+
+fig.show()
+#Now let’s have a look at the sales by sub-category:
+sales_by_subcategory = data.groupby('Sub-Category')['Sales'].sum().reset_index()
+fig = px.bar(sales_by_subcategory, 
+             x='Sub-Category', 
+             y='Sales', 
+             title='Sales Analysis by Sub-Category')
+fig.show()
+#Now let’s have a look at the monthly profits:
+profit_by_month = data.groupby('Order Month')['Profit'].sum().reset_index()
+fig = px.line(profit_by_month, 
+              x='Order Month', 
+              y='Profit', 
+              title='Monthly Profit Analysis')
+fig.show()
+#Now let’s have a look at the profit by category:
+profit_by_category = data.groupby('Category')['Profit'].sum().reset_index()
+
+fig = px.pie(profit_by_category, 
+             values='Profit', 
+             names='Category', 
+             hole=0.5, 
+             color_discrete_sequence=px.colors.qualitative.Pastel)
+
+fig.update_traces(textposition='inside', textinfo='percent+label')
+fig.update_layout(title_text='Profit Analysis by Category', title_font=dict(size=24))
+
+fig.show()
+#Now let’s have a look at the profit by sub-category:
+profit_by_subcategory = data.groupby('Sub-Category')['Profit'].sum().reset_index()
+fig = px.bar(profit_by_subcategory, x='Sub-Category', 
+             y='Profit', 
+             title='Profit Analysis by Sub-Category')
+fig.show()
+#Now let’s have a look at the sales and profit analysis by customer segments:
+sales_profit_by_segment = data.groupby('Segment').agg({'Sales': 'sum', 'Profit': 'sum'}).reset_index()
+
+color_palette = colors.qualitative.Pastel
+
+fig = go.Figure()
+fig.add_trace(go.Bar(x=sales_profit_by_segment['Segment'], 
+                     y=sales_profit_by_segment['Sales'], 
+                     name='Sales',
+                     marker_color=color_palette[0]))
+fig.add_trace(go.Bar(x=sales_profit_by_segment['Segment'], 
+                     y=sales_profit_by_segment['Profit'], 
+                     name='Profit',
+                     marker_color=color_palette[1]))
+
+fig.update_layout(title='Sales and Profit Analysis by Customer Segment',
+                  xaxis_title='Customer Segment', yaxis_title='Amount')
+fig.show()
+#So the store has higher profits from the product sales for consumers, but the profit from corporate product sales is better in the sales-to-profit ratio
+#Let’s have a look at it to validate our findings:
+sales_profit_by_segment = data.groupby('Segment').agg({'Sales': 'sum', 'Profit': 'sum'}).reset_index()
+sales_profit_by_segment['Sales_to_Profit_Ratio'] = sales_profit_by_segment['Sales'] / sales_profit_by_segment['Profit']
+print(sales_profit_by_segment[['Segment', 'Sales_to_Profit_Ratio']])
